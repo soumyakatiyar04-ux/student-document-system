f2caaf1266f8a898345878ba6272b2138c0af9c9
soumya katiyar <soumyakatiyar04@gmail.com>
2026-09-26 01:42:02 +0530

    third commit

--- src/controller/documentController.js
+++ src/controller/documentController.js
@@ -1,11 +1,11 @@
 const connection = require("../config/db");
 const { uploadDocument } = require("../services/s3Service");
 
-const { uploadM } = require("@aws-sdk/client-cloudwatch");
-
 const getDocuments = async (req, res) => {
-  let query = "SELECT * FROM documents";
+  const query = "SELECT * FROM documents";
+
   const [result] = await connection.query(query);
+
   res.json({
     message: "Data fetched successfully",
     data: result,
@@ -13,8 +13,10 @@ const getDocuments = async (req, res) => {
 };
 
 const getElementById = async (req, res) => {
-  let query = "SELECT * FROM documents WHERE id = ?";
+  const query = "SELECT * FROM documents WHERE id = ?";
+
   const [result] = await connection.query(query, [req.params.id]);
+
   res.json({
     message: "Data fetched successfully",
     data: result,
@@ -22,8 +24,13 @@ const getElementById = async (req, res) => {
 };
 
 const getElementByUserId = async (req, res) => {
-  let query = "SELECT * FROM documents WHERE user_id = ?";
-  const [result] = await connection.query(query, [req.params.user_id]);
+  const query = "SELECT * FROM documents WHERE user_id = ?";
+
+  const [result] = await connection.query(
+    query,
+    [req.params.user_id]
+  );
+
   res.json({
     message: "Data fetched successfully",
     data: result,
@@ -31,26 +38,26 @@ const getElementByUserId = async (req, res) => {
 };
 
 const postDocuments = async (req, res) => {
-  let profile = req.file;
-  let user_id = req.body.user_id;
+  const profile = req.file;
+  const user_id = req.body.user_id;
 
   if (!profile || !user_id) {
-    return res.json({
+    return res.status(400).json({
       message: "File and user_id are required",
     });
   }
 
-  // Upload file to S3
-  let s3Data = await uploadDocument(profile, user_id);
+  // 1. Upload file to S3
+  const s3Data = await uploadDocument(profile, user_id);
 
-  // Insert data into database
-  let query = `
-        INSERT INTO documents
-        (s3_key, s3_url, file_size, mime_type, original_name, user_id)
-        VALUES (?, ?, ?, ?, ?, ?)
-    `;
+  // 2. Save S3 information in MySQL
+  const query = `
+    INSERT INTO documents
+    (s3_key, s3_url, file_size, mime_type, original_name, user_id)
+    VALUES (?, ?, ?, ?, ?, ?)
+  `;
 
-  const [result] = await connection.execute(query, [
+  await connection.execute(query, [
     s3Data.s3_key,
     s3Data.s3_url,
     profile.size,
@@ -61,10 +68,12 @@ const postDocuments = async (req, res) => {
 
   console.log("Data inserted successfully");
 
-  res.json({
+  res.status(201).json({
     message: "Document uploaded successfully",
+    data: s3Data,
   });
 };
+
 module.exports = {
   getDocuments,
   postDocuments,
